596f30a60708b728a4d0ad3d8e177f39b1201bbc
margo-lee <margo.lee@hyweb.com.tw>
2023-11-21 14:00:50 +0800

    fix:login頁面新增cookie function

--- src/views/layout/loginPage.vue
+++ src/views/layout/loginPage.vue
@@ -52,6 +52,8 @@ export default {
   data: () => ({
     visible: false,
     checkbox: false,
+    theme: "",
+    dark: false,
   }),
 
   methods: {
@@ -66,9 +68,44 @@ export default {
         { y: 0, duration: 1, opacity: 1 }
       );
     },
+    readCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
+    },
+    createCookie(name, value, days) {
+      let _expires;
+      const _date = new Date();
+      if (days) {
+        _date.setTime(_date.getTime() + days * 24 * 60 * 60 * 1000);
+        _expires = "; expires=" + _date.toGMTString();
+      } else {
+        _expires = "";
+      }
+      document.cookie = name + "=" + value + _expires + "; path=/";
+    },
+    changeTheme(color) {
+      let th = this.$vuetify.theme.global;
+      th.name = color;
+      this.theme = color;
+      this.themeDark = false;
+      this.createCookie("Theme", `${color}`, 356);
+    },
+    themeInit() {
+      this.theme = this.readCookie("Theme") || null;
+      if (this.theme == null) {
+        this.theme = "default";
+        this.themeDark = false;
+      }
+      if (this.theme == "dark") {
+        this.themeDark = true;
+      }
+      this.changeTheme(this.theme);
+    },
   },
   mounted() {
     this.cardAnimate();
+    this.themeInit();
   },
 };
 </script>
